21399ad5b4b02de18e20a1ebba544e84e4b90782
Michael Ferger <mferger92@gmail.com>
2016-02-16 10:22:57 -0700

    pushing random code

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
-for (var i = 0; i < 3000; i++) {
-  var body = document.querySelector('body');
-  body.style.marginLeft = '-4px';
-  body.style.marginRight = '-4px';
-  body.style.marginTop = '-4px';
-  body.style.marginBottom = '-4px';
+var body  = document.querySelector('body');
+for (var i = 0; i < 2000; i++) {
+var newDiv = document.createElement('div');
 
-  var redBox = document.createElement('div');
-  redBox.style.width = '11.1%';
-  redBox.style.float = 'left';
-  redBox.style.paddingBottom = '11.1%';
-  redBox.style.backgroundColor = 'red';
+body.style.marginLeft = "-4px";
+body.style.marginRight = "-4px";
+body.style.marginTop = "-4px";
+body.style.marginBottom = "-4px";
 
-  var blackBox = document.createElement('div');
-  blackBox.style.width = '11.1%';
-  blackBox.style.float = 'left';
-  blackBox.style.paddingBottom = '11.1%';
-  blackBox.style.backgroundColor = 'black';
+body.appendChild(newDiv);
 
-  if (i % 2 == 0) {
-    body.appendChild(blackBox);
-  } else {
-    body.appendChild(redBox);
+newDiv.style.width = "11.1%";
+newDiv.style.paddingBottom = "11.1%";
+newDiv.style.float = "left";
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
-};
-
-
-
-
-
-
-
-
-
-// var grid = document.createElement('grid');
-// grid.width = grid.height = 320;
-// var context = grid.getContext('2d');
-// for (var x = 0; x < 16; x++) for (var y = 0; y < 16; y++) {
-//   context.fillStyle = (x + y) % 2 ? 'red' : 'black';
-//   context.fillRect(40 * x, 40 * y, 40, 40)
-// }
-// document.body.appendChild(grid);
+newDiv.style.backgroundColor = getRandomColor();
+}
